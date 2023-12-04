b7b0928d38b0864adbfb8d3067423cdf6886442f
IntobuKobu <145839313+IntobuKobu@users.noreply.github.com>
2023-12-04 18:24:14 +0700

    Create Version.lua

---
+++ Version.lua
@@ -0,0 +1,7 @@
+local Version = {
+    ["The Mimic"] = "UPV1D",
+    ["Dragon Adventure"] = "UPV1D",
+    ["Blox Fruit"] = "UPV1D",
+    ["Pet Simulator 99"] = "UPV1D",
+    ["Weird Stitch Dad"] = "UPV1D",
+}
